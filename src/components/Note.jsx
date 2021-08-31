ee727b30d197e600108f82a2c76f1be7dfca9e4a
Tim Shultz <ttshultz@gmail.com>
2021-08-31 08:00:26 -0700

    Render notes

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -2,11 +2,11 @@
 //<h1> for a title and a <p> for the content.
 import React from "react";
 
-function Note() {
+function Note(props) {
     return(
     <div className="note">
-        <h1>This is the note title</h1>
-        <p>This is the note content</p>
+        <h1>{props.title}</h1>
+        <p>{props.content}</p>
     </div>);
 }
 
